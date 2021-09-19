d0e54044c8015b2cce6e83c8de5a1836852d1e1c
Luke Becker <luke.w.becker32@gmail.com>
2021-09-19 08:09:02 -0500

    removal

--- cbp_env.py
+++
@@ -1,5 +0,0 @@
-# info for CBPro
-
-passphrase = '597zl9qsgqs'
-api_key = 'e07e1cf972e7c601991be1babcaa47b6'
-api_secret = 'E4fzNlkYuVVs8IaxM+bsESAvGRdA94sT+JRSlB9zc0J82OIknIvVljyI2knvztAFqU4q6rufIP3dWTPd+BaKeg=='
