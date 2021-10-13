e79a5b1ab0b2e2da1aae086babbe6a278c268362
Luke Becker <luke.w.becker32@gmail.com>
2021-10-13 07:33:45 -0500

    updates

---
+++ crontab_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from datetime import datetime
+import os
+sysDate = datetime.now()
+# convert system date to string
+curSysDate = (str(sysDate))
+# parse the date only from the string
+curDate = curSysDate[0:10]
+# parse the hour only from the string
+curHour = curSysDate[11:13]
+# parse the minutes only from the string
+curMin = curSysDate[14:16]
+# concatenate hour and minute with underscore
+curTime = curHour + '_' + curMin
+# val for the folder name
+folderName = curDate + '_' + curTime
+# make a directory
+os.mkdir(folderName)
